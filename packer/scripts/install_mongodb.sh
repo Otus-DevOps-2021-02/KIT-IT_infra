dead129ab85c88cf9d6592d01cfa6da79b16dfc8
user <user@localhost.localdomain>
2021-05-07 20:54:14 +0300

    ansible-2

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -6,13 +6,17 @@ apt-get install apt-transport-https
 sleep 30
 apt-get install gnupg
 sleep 30
+echo "wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc"
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
 sleep 30
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sleep 30
+echo "apt-get update"
 apt-get update
-sleep 30
+sleep 50
+echo "apt install -y mongodb-org"
 apt install -y mongodb-org
 sleep 30
+echo "systemctl start mongod"
 systemctl start mongod
 systemctl enable mongod
